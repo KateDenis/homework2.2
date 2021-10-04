34b5f86ef0213f8d9d424faa1388c9c64215a0ba
Kate <pink-angel@mail.ru>
2021-10-04 20:04:55 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        double accountBalance = 100;
-        double refilling = 700;
+        int accountBalance = 100;
+        int refilling = 1100;
         int bonusStep = 100;
         if (refilling >= 1000) {
-            int bonus = (int) refilling / bonusStep;
+            int bonus = refilling / bonusStep;
             System.out.println(accountBalance + refilling + bonus);
         } else {
             int bonus = 0;
